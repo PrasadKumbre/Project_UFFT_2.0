e88fc4db38421a21f2c2b820f45d13072c6f4c63
Prasad Kumbre <104724818+PrasadKumbre@users.noreply.github.com>
2025-04-19 17:12:57 +0530

    Add files via upload

---
+++ db_connection.py
@@ -0,0 +1,36 @@
+# db_connection.py
+import mysql.connector
+from mysql.connector import Error
+
+
+def get_connection():
+    """Creates and returns a new database connection."""
+    try:
+        connection = mysql.connector.connect(
+            host='sql12.freesqldatabase.com',          # Replace with your database host
+            user='sql12774113',          # Replace with your database username
+            password='Vl6G3Mdisr',  # Replace with your database password
+            database='sql12774113'   # Replace with your database name
+        )
+        # connection = mysql.connector.connect(
+        #     host='localhost',          # Replace with your database host
+        #     user='root',          # Replace with your database username
+        #     password='root',  # Replace with your database password
+        #     database='projectufft'   # Replace with your database name
+        # )
+        
+        return connection
+    except Error as e:
+        print(f"Error connecting to database: {e}")
+        raise
+
+###### Creating session ################# do not remove it is imp ####
+def create_session(session,cur,login_name):
+    session['login_name'] = login_name
+    cur.execute("SELECT user_id, role, family_id, name FROM users WHERE user_name = %s", (login_name,))
+    user_data = cur.fetchone()
+    if user_data:
+        session['user_id'], session['role'], session['family_id'], session['name'] = user_data
+    else:
+        print("User not found.")
+
